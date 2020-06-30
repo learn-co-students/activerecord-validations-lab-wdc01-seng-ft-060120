faba5dd994042940a91ea2cf4530d7c7394001b6
Tien Phan <tienphan1989@gmail.com>
2020-06-30 01:54:29 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ActiveRecord::Base
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not valid" }
+
+    validate :if_clickbait, if: :title
+
+    def if_clickbait
+        words = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+        words.each do |word|
+        if title.include?(word)
+            return true
+            end
+        end
+            errors[:title] << 'more clickbait'
+        return false
+    end
 end
+
+
+
